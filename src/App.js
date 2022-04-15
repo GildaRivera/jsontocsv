691fbdfb069b458b83a9fe9f38cee892be486c1d
gildarivera <gilda.rivera@telus.com>
2022-04-15 16:33:57 -0600

    feat: changing styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Button, Container, TextField } from "@mui/material";
 import { Box } from "@mui/system";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   const [csv, setCSV] = useState([]);
-  const [jsonA, setJsonA] = useState()
+  const [jsonA, setJsonA] = useState();
+  const jsonInput = useRef();
+  const csvInput = useRef();
   const handleConvet = (arr) => {
     const array = [Object.keys(arr[0])].concat(arr);
-
     return array
       .map((it) => {
         return Object.values(it).toString();
@@ -19,23 +20,41 @@ function App() {
   const convert = () => {
     setCSV(handleConvet(jsonA));
   };
-  const handleChange = (e)=>{
- console.log( JSON.parse(e.target.value), )
-    //  Array.from(e.target.value)
-setJsonA(JSON.parse(e.target.value))
-  }
+  const handleChange = (e) => {
+    try {
+      setJsonA(JSON.parse(e.target.value));
+    } catch (e) {}
+  };
+  const handleClean = () => {
+    jsonInput.current.value = "";
+    csvInput.current.value = "";
+    setCSV();
+    setJsonA();
+    //setFormat();
+  };
+  const handleFormat = (e) => {
+    //setFormat(JSON.stringify(jsonA, null, 2));
+    jsonInput.current.value = JSON.stringify(jsonA, null, 2);
+  };
   return (
+    <>
+    <h1 class="title is-size-1" style={{textAlign:"center", color:"white"}}>JSON to CSV</h1>
+
     <div style={{ display: "flex" }}>
-      <Container maxWidth="sm" sx={{ height: "90vh", margin: "2vh" }}>
+      <Container maxWidth="sm" sx={{ height: "60vh", margin: "2vh" }}>
         <Box
           sx={{
-            bgcolor: "#cfe8fc",
+            
             height: "100vh",
             position: "relative",
             height: "100%",
           }}
         >
-          <textarea onChange={handleChange} style={{"height":"100%", "width":"100%"}}/>
+          <textarea
+            onChange={handleChange}
+            ref={jsonInput}
+            style={{ height: "100%", width: "100%", borderRadius:"10px", backgroundColor:"#white",color:"#1976d2", fontSize:"larger" }}
+          />
         </Box>
       </Container>
       <div
@@ -49,24 +68,35 @@ setJsonA(JSON.parse(e.target.value))
         <Button variant="contained" sx={{ margin: "2vh 0" }} onClick={convert}>
           Tranform
         </Button>
-        <Button variant="contained">Format</Button>
-        <Button variant="contained" sx={{ margin: "2vh 0" }}>
+        <Button variant="contained" onClick={handleFormat} sx={{   }}>
+          Format
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ margin: "2vh 0" }}
+          onClick={handleClean}
+        >
           Clean
         </Button>
       </div>
-      <Container maxWidth="sm" sx={{ height: "90vh", margin: "2vh" }}>
+      <Container maxWidth="sm" sx={{ height: "60vh", margin: "2vh" }}>
         <Box
           sx={{
-            bgcolor: "#cfe8fc",
+            
             height: "100vh",
             position: "relative",
             height: "100%",
           }}
         >
-          <textarea value={csv} disabled style={{"height":"100%", "width":"100%"}}/>
+          <textarea
+            value={csv}
+            ref={csvInput}
+            disabled
+            style={{ height: "100%", width: "100%",borderRadius:"10px", backgroundColor:"rgb(0 142 255 / 7%)", color:"#1976d2", fontSize:"larger" }}
+          />
         </Box>
       </Container>
-    </div>
+    </div>    </>
   );
 }
 
